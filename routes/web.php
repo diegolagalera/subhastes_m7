faf156f1297fe739852897255ea6fa3dfc2ca5f3
ssjordiss <34072263+ssjordiss@users.noreply.github.com>
2018-03-20 18:59:48 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-=======
- 
->>>>>>> c5f530fa6b7190f1d53764ed76580877496bd88b
