9ad8cbcdbc9af0efa45bf59f0174d88226767a38
ssjordiss <34072263+ssjordiss@users.noreply.github.com>
2018-03-22 17:09:22 +0100

    productes

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,24 @@
+@extends("layouts.app");
+
+@section("content")
+  <div class="big-padding text-center blue-grey white-text">
+    <h1>Productes</h1>
+  </div>
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($products as $product)
+          <tr>
+            <td>{{$product->id}}</td>
+            <td>{{$product->nom}}</td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/productes','productescontroller@index')->name('articles');
+
+Route::get('/productes/crear','productescontroller@create')->name('crearart');
