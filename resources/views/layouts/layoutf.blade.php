944d48503774e463e235e161fb66355d88cb2cb6
diegolagalera <diegomartinezmartinez@iesmontsia.org>
2018-04-17 18:26:43 +0200

    asd

--- resources/views/layouts/layoutf.blade.php
+++ resources/views/layouts/layoutf.blade.php
@@ -62,10 +62,13 @@
                           <a href="./index.html" class="dropdown-item">
                               <i class="material-icons"></i> Profile
                           </a>
-                          <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                              <i class="material-icons"></i> {{ __('Logout') }}
-                          </a>
+                            <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();">
+                                <i class="material-icons"></i> {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                       </div>
                   </li>
 
