fb8b94fb76b7e453b7b96626cbe2ba3ab239cbde
adrianmontoro <adrianmontoro@iesmontsia.org>
2018-04-19 17:27:59 +0200

    PDF acabat

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -93,6 +93,7 @@
         <a href="{{ url('subhastes/'.$comp->id_subhasta) }}" class="btn btn-success">Veure</a>
         </td>
         <td>
+        <a href="/pdf/{{$comp->id}}"><i style="font-size:30px;" class="fa fa-file-pdf-o "></i></a>
       </td>
     </tr>
     @endforeach
